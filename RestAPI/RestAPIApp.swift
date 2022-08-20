e5083f2f6c28672c94c19a6125dcd8eb3bb1100f
muhfikrihadian <muhfikrihadian@gmail.com>
2022-08-21 04:55:31 +0700

    Initial Commit

---
+++ RestAPI/RestAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestAPIApp.swift
+//  RestAPI
+//
+//  Created by Muhammad Fikri Hadian on 21/08/22.
+//
+
+import SwiftUI
+
+@main
+struct RestAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
